786e7ee8cf59ce08bb29a18f1202d13423ac1ec9
valeriaramirezg <valeria.ramirezg@edu.uah.es>
2025-05-05 08:13:18 +0000

    CAMBIOS APP IMAGENES SCALA

--- models.py
+++ models.py
@@ -3,30 +3,11 @@ from sqlalchemy.orm import validates
 
 from app import db
 
+class ImageCloud(db.Model):
+    __tablename__ = 'image_cloud'
 
-class Restaurant(db.Model):
-    __tablename__ = 'restaurant'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    street_address = Column(String(50))
-    description = Column(String(250))
-
-    def __str__(self):
-        return self.name
-
-class Review(db.Model):
-    __tablename__ = 'review'
-    id = Column(Integer, primary_key=True)
-    restaurant = Column(Integer, ForeignKey('restaurant.id', ondelete="CASCADE"))
-    user_name = Column(String(30))
-    rating = Column(Integer)
-    review_text = Column(String(500))
-    review_date = Column(DateTime)
-
-    @validates('rating')
-    def validate_rating(self, key, value):
-        assert value is None or (1 <= value <= 5)
-        return value
-
-    def __str__(self):
-        return f"{self.user_name}: {self.review_date:%x}"
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    username = db.Column(db.Text, nullable=False)
+    filename = db.Column(db.Text, nullable=False)
+    pixels = db.Column(db.Text, nullable=False)
